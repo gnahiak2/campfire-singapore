74b959695bb7a0393de64a4e0be2720aa567d8eb
ascpixi <44982772+ascpixi@users.noreply.github.com>
2025-12-09 12:58:10 -0500

    fix: remove Vite image optimizer

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
-    ViteImageOptimizer({
-      png: {
-        effort: 10,
-        compressionLevel: 9,
-        progressive: true
-      },
-      ansiColors: true,
-      logStats: true
-    })
-  ],
+    tailwindcss()
+  ]
 });
